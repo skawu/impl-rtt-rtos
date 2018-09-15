68801fd60c66234f52cd3b5eb0e72655db496b00
skawu <maoxudong0813@163.comm>
2018-09-15 09:02:38 +0800

    裸机模拟实现keil逻辑分析仪两个任务交替

---
+++ User/main.c
@@ -0,0 +1,31 @@
+
+/*
+ *	main.c нд╪Ч
+ */
+#include <stdio.h>
+
+typedef unsigned int uint32_t;
+
+uint32_t flag1;
+uint32_t flag2;
+
+void delay(uint32_t count)
+{
+	for(; count != 0; count--);
+}
+
+int main(void)
+{
+	for(;;)
+	{
+		flag1 = 1;
+		delay(100);
+		flag1 = 0;
+		delay(100);
+		
+		flag2 = 1;
+		delay(100);
+		flag2 = 0;
+		delay(100);
+	}
+}
